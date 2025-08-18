b60e278c0654ed30e68c0706c7537e7cd4ce8b5b
Nguyen The Bach <178692316+djgmh9@users.noreply.github.com>
2025-08-18 13:43:38 +0800

    Add list

--- src/main/java/Frenny.java
+++ src/main/java/Frenny.java
@@ -1,22 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Frenny {
+    private static List<String> items = new ArrayList<>();
+
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n" +
+        String intro = "____________________________________________________________\n" +
                 " Hello! I'm Frenny\n" +
-                " What can I do for you?\n";
-        System.out.println(logo);
+                " What can I do for you?\n" +
+                "____________________________________________________________\n";
+        System.out.println(intro);
         while (true) {
-            System.out.print("Enter a command: ");
             Scanner s = new Scanner(System.in);
             String input = s.nextLine();
                     System.out.println("____________________________________________________________\n");
             if (Objects.equals(input, "bye")) {
                 System.out.println("Bye. Hope to see you again soon!\n");
                 break;
+            } else if (Objects.equals(input, "list")) {
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println((i + 1) + ". " + items.get(i));
+                }
+            } else {
+                System.out.println("added: " + input + "\n");
             }
-            System.out.println(input);
+            items.add(input);
             System.out.println("____________________________________________________________\n");
         }
     }
