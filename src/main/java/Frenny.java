18e72eae75d16edb7fe654d38292d4b006f35365
Nguyen The Bach <178692316+djgmh9@users.noreply.github.com>
2025-08-18 13:00:19 +0800

    Skeletal version

--- src/main/java/Frenny.java
+++ src/main/java/Frenny.java
@@ -1,11 +1,23 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Frenny {
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n" +
                 " Hello! I'm Frenny\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+                " What can I do for you?\n";
         System.out.println(logo);
+        while (true) {
+            System.out.print("Enter a command: ");
+            Scanner s = new Scanner(System.in);
+            String input = s.nextLine();
+                    System.out.println("____________________________________________________________\n");
+            if (Objects.equals(input, "bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                break;
+            }
+            System.out.println(input);
+            System.out.println("____________________________________________________________\n");
+        }
     }
 }
