b65e75a73ddf47ea10f03d746ad134091c36c62b
Nguyen The Bach <178692316+djgmh9@users.noreply.github.com>
2025-08-18 17:09:10 +0800

    Implement mark as done feature

--- src/main/java/Frenny.java
+++ src/main/java/Frenny.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Frenny {
-    private static List<String> items = new ArrayList<>();
+    private static List<Task> items = new ArrayList<>();
 
     public static void main(String[] args) {
         String intro = "____________________________________________________________\n" +
@@ -14,7 +14,7 @@ public class Frenny {
         System.out.println(intro);
         while (true) {
             Scanner s = new Scanner(System.in);
-            String input = s.nextLine();
+            String input = s.next();
                     System.out.println("____________________________________________________________\n");
             if (Objects.equals(input, "bye")) {
                 System.out.println("Bye. Hope to see you again soon!\n");
@@ -23,10 +23,24 @@ public class Frenny {
                 for (int i = 0; i < items.size(); i++) {
                     System.out.println((i + 1) + ". " + items.get(i));
                 }
+            } else if (Objects.equals(input, "mark")) {
+                System.out.println("Nice! I've marked this task as done:");
+                int taskNumber = Integer.parseInt(s.next()) - 1;
+                Task task = items.get(taskNumber);
+                task.mark();
+                System.out.println(task);
+            } else if (Objects.equals(input, "unmark")) {
+                System.out.println("OK, I've marked this task as not done yet:");
+                int taskNumber = Integer.parseInt(s.next()) - 1;
+                Task task = items.get(taskNumber);
+                task.unmark();
+                System.out.println(task);
             } else {
-                System.out.println("added: " + input + "\n");
+                String item = input + s.nextLine();
+                Task task = new Task(item);
+                items.add(task);
+                System.out.println("added: " + item + "\n");
             }
-            items.add(input);
             System.out.println("____________________________________________________________\n");
         }
     }
