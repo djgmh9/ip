05e8b101c6f80cfdc0c316fa34c7e6a58c9c7bde
Nguyen The Bach <178692316+djgmh9@users.noreply.github.com>
2025-08-18 09:53:18 +0800

    Add greetings and rename

---
+++ src/main/java/Frenny.java
@@ -0,0 +1,11 @@
+public class Frenny {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n" +
+                " Hello! I'm Frenny\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(logo);
+    }
+}
